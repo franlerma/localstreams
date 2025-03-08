d43ff3ce17ab16fec21905cee33839e00fe36f52
Fran Lerma <deeex87@users.noreply.github.com>
2025-03-08 17:31:30 +0100

    Update localstream.py

--- app/localstream.py
+++ app/localstream.py
@@ -401,13 +401,11 @@ async def ace_stream(request: Request):
             except (aiohttp.ClientError, asyncio.TimeoutError) as e:
                 attempt += 1
                 logger.warning(f"Intento {attempt} fallido: {str(e)}")
-                # backoff = ACESTREAM_RETRY_BACKOFF_FACTOR ** (attempt + 1)
-                # await asyncio.sleep(backoff)
                 
                 if attempt >= ACESTREAM_RETRY_TOTAL and not manager.check_health():
                     attempt = 0
                     await manager.restart_service()
-                    await asyncio.sleep(ACESTREAM_RETRY_BACKOFF_FACTOR)
+                    await asyncio.sleep(3000)
     
                 
 
