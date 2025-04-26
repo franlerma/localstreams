91614d9d4644d778dc5d18a357c6078aa9a77381
Fran Lerma <deeex87@users.noreply.github.com>
2025-04-26 16:23:58 +0200

    fix health method

--- app/localstream.py
+++ app/localstream.py
@@ -5,6 +5,7 @@ import shutil
 import signal
 import logging
 import time
+import requests
 from contextlib import asynccontextmanager
 from typing import Optional
 from typing import AsyncGenerator
@@ -106,18 +107,14 @@ class AceStreamManager:
 
     async def check_health(self) -> bool:
         try:
-            async with self.http_session.get(
+            with requests.get(
                 f"http://{ACESTREAM_PROXY_HOST}:{ACESTREAM_PROXY_PORT}/ace/status",
                 timeout=ClientTimeout(total=3)
             ) as response:
-                return response.status == 200
-                # if response.status != 200:
-                #     return False
-                
-                # data = await response.json()
-                # logger.debug(f"Respuesta de salud: {data}")
+                if response.status_code != 200:
+                     return False
 
-                # return data.get("error") is None
+                return response.json().get("error") is None
         except Exception as e:
             logger.debug(f"Error de salud: {str(e)}")
             return False
@@ -337,7 +334,7 @@ async def ace_stream(request: Request):
         while True: 
             try:
                 async with aiohttp.ClientSession() as session:
-                    async with session.get(acestream_url) as response:
+                    with session.get(acestream_url) as response:
                         if response.status == 200:
                             while True:
                                 chunk = await response.content.read(8192)
